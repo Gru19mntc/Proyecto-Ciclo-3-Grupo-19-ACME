950409e59ca97bd8152f6a6b76b77e0eabeed7b6
Juansis99 <jsimoncardona@hotmail.com>
2022-08-17 21:35:23 -0500

    Creacion del proyecto

---
+++ ProyectoCiclo3ColaborativoGrupo19Acme/src/Integrantes.java
@@ -0,0 +1,27 @@
+public class Integrantes {
+    //Esta Clase se crea con la finalidad de sustentar el primer sprint del proyecto del ciclo 3 de programacion web en
+    //Para el Sprint 1 se tiene 3 tareas:
+    //1. Conformar el equipo de trabajo - El equipo tiene 5 personas
+    //2. Creacion de una organizacion en GitHub - Existe un link para la organizacion del Github del equipo
+    //3. Creacion de un repositorio de ejemplo dentro de la organizacion - donde cada miembro evidencie un commit - es
+    //                                                                     posible evidenciar al menos un commit de cada
+    //                                                                     miembro en un repositorio dentro de la
+    //                                                                     organizacion.
+    //Desarrollo del Sprint #1 para el ciclo 3 de mision TIC 2022
+    //1. el equipo esta conformado por las siguientes 5 personas:
+    //1.1
+    //1.2
+    //1.3
+    //1.4
+    //1.5
+    //
+    //2. Se crea una organizacion en Github
+    //2.1 La organizacion creada en Github (Usuario) es "Gru19mntc"
+    //2.2 El link para la organizacion del grupo es: "https://github.com/Gru19mntc"
+    //
+    //3. Se crea un repositorio de ejemplo dentro de la organizacion
+    //3.1 El repositorio dentro de la organizacion se llama: "Proyecto-Ciclo-3-Grupo-19-ACME"
+    //    su ruta de acceso es "https://github.com/Gru19mntc/Proyecto-Ciclo-3-Grupo-19-ACME"
+    //3.2 los commits de cada uno de los integrantes se evidenciaran al ir llenando en el punto 1 los nombres de cada
+    //    uno de los integrantes.
+}
