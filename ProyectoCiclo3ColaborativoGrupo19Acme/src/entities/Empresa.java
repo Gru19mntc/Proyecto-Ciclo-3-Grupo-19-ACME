497af288907bd17cf79499ccb6729f44f21d8902
Joaokld3 <juankld3@gmail.com>
2022-08-22 22:39:54 -0500

    Se instancia la clase Empresa junto a sus atributos, método constructor, setters y getter. Se crea el main para pruebas

--- ProyectoCiclo3ColaborativoGrupo19Acme/src/entities/Empresa.java
+++ ProyectoCiclo3ColaborativoGrupo19Acme/src/entities/Empresa.java
@@ -1,5 +1,41 @@
 package entities;
 
 public class Empresa {
-    //Creacion de la Clase Empresa
+
+    private String name;
+    private String adress;
+    private int telf;
+    private int nit;
+    //Constructor de la claseEmpresa
+    public Empresa(String name, String adress, int telf, int nit) {
+        this.name = name;
+        this.adress = adress;
+        this.telf = telf;
+        this.nit = nit;
+    }
+    //Métodos Getters y Setter, los getter obtienen los datos de la propiedad, los setter cambian los valores
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAdress() {
+        return adress;
+    }
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    public int getTelf() {
+        return telf;
+    }
+    public void setTelf(int telf) {
+        this.telf = telf;
+    }
+    public int getNit() {
+        return nit;
+    }
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
 }
