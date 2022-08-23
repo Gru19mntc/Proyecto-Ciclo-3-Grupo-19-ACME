5f0f0c0d84e720189cb84745a70709d0881bc4cc
hdguevara <arido2@gmail.com>
2022-08-22 22:40:51 -0500

    "Creacion de las variables en la clase empleados"

--- ProyectoCiclo3ColaborativoGrupo19Acme/src/entities/Empleado.java
+++ ProyectoCiclo3ColaborativoGrupo19Acme/src/entities/Empleado.java
@@ -2,4 +2,61 @@ package entities;
 
 public class Empleado {
     //Creacion de la Clase Empleado
+    private int idEmpleado;
+    private String nombrEmpleado;
+    private String correoEmpleado;
+    private String empresaEmpleado;
+    private String rolEmpleado;
+
+    public Empleado(int idEmpleado, String nombrEmpleado, String correoEmpleado, String empresaEmpleado, String rolEmpleado) {
+        this.idEmpleado = idEmpleado;
+        this.nombrEmpleado = nombrEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.empresaEmpleado = empresaEmpleado;
+        this.rolEmpleado = rolEmpleado;
+
+    }
+
+    public Empleado() {
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombrEmpleado() {
+        return nombrEmpleado;
+    }
+
+    public void setNombrEmpleado(String nombrEmpleado) {
+        this.nombrEmpleado = nombrEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(String empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
 }
