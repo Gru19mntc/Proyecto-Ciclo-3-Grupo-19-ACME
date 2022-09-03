f978c3e22cf5a611553000f8f9fb9c6c05764b8d
Juan Diaz <107194287+Joaokld3@users.noreply.github.com>
2022-09-03 10:17:46 -0500

    Se realiza modificaciones segun diagrama entidad relacion de la entidad empresa

--- src/main/java/ProyectoCiclo3ColaborativoGrupo19Acme/src/entities/Empresa.java
+++ src/main/java/ProyectoCiclo3ColaborativoGrupo19Acme/src/entities/Empresa.java
@@ -1,28 +1,57 @@
-package entities;
-
+package ProyectoCiclo3ColaborativoGrupo19Acme.src.entities;
+import entities.Empleado;
+import entities.MovimientoDinero;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table
+@NoArgsConstructor
 public class Empresa {
-
-    private String name;
+    @Id    private long id;
+    @Column(name = "name", unique = true) private String name;
+    @Column(name = "document", unique = true) private String document;
+    private String phone;
     private String adress;
-    private int telf;
-    private int nit;
+    @OneToMany private entities.Empleado empleado;
+    @OneToMany private entities.MovimientoDinero movimientoDinero;
+    private Date createdAt;
+    private Date updatedAt;
     //Constructor de la claseEmpresa
-    public Empresa(String name, String adress, int telf, int nit) {
+    public Empresa(long id, String name, String document, String phone, String adress, entities.Empleado empleado, entities.MovimientoDinero movimientoDinero, Date createdAt, Date updatedAt) {
+        this.id = id;
         this.name = name;
+        this.document = document;
+        this.phone = phone;
         this.adress = adress;
-        this.telf = telf;
-        this.nit = nit;
+        this.empleado = empleado;
+        this.movimientoDinero = movimientoDinero;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
-
-    public Empresa(){
-        this.name = "Empresa Generica";
+    public long getId() {
+        return id;
     }
-    //Métodos Getters y Setter, los getter obtienen los datos de la propiedad, los setter cambian los valores
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public String getDocument() {
+        return document;
+    }
+    public void setDocument(String document) {
+        this.document = document;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
     public String getAdress() {
         return adress;
@@ -30,16 +59,28 @@ public class Empresa {
     public void setAdress(String adress) {
         this.adress = adress;
     }
-    public int getTelf() {
-        return telf;
+    public Empleado getEmpleado() {
+        return empleado;
     }
-    public void setTelf(int telf) {
-        this.telf = telf;
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
     }
-    public int getNit() {
-        return nit;
+    public MovimientoDinero getMovimientoDinero() {
+        return movimientoDinero;
     }
-    public void setNit(int nit) {
-        this.nit = nit;
+    public void setMovimientoDinero(MovimientoDinero movimientoDinero) {
+        this.movimientoDinero = movimientoDinero;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
