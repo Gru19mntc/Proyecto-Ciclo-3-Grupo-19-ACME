7afb6c1f5c8150302d447e9412928afc2e9d346d
Juansis99 <jsimoncardona@hotmail.com>
2022-09-24 00:23:27 -0500

    Extencion de seguridad home e index

--- src/main/java/com/example/ProyectoCiclo3Grupo19ACME/controllers/FrontController.java
+++ src/main/java/com/example/ProyectoCiclo3Grupo19ACME/controllers/FrontController.java
@@ -31,8 +31,20 @@ public class FrontController {
     }
 
     //Metodos
+    public void getOrSetUserRol(Model model, OidcUser principal){
+        if(principal != null){
+            String emailLogin = (String) (principal.getClaims()).get("email");
+            Empleado empleadoLogin = empleadoService.findEmpleadoByCorreoEmpleado(emailLogin);
+            if(empleadoLogin == null){
+                empleadoLogin = new Empleado(0,principal.getNickName(),emailLogin,EmpleadoRol.Default);
+            }
+            model.addAttribute("empleadoLogin", empleadoLogin);
+        }
+    }
+
     @GetMapping("/")
     public String index(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         if(principal != null){
             User user = this.userService.getOrCreateUser(principal.getClaims());
             model.addAttribute("user", user);
@@ -42,62 +54,65 @@ public class FrontController {
 
     @GetMapping("/home")
     public String home(Model model, @AuthenticationPrincipal OidcUser principal){
-        String emailLogin = principal.getEmail();
-        Empleado empleadoLogin = empleadoService.findEmpleadoByCorreoEmpleado(emailLogin);
-        if(empleadoLogin == null){
-            empleadoLogin = new Empleado(0,principal.getNickName(),emailLogin,EmpleadoRol.Default);
-        }
-        model.addAttribute("empleadoLogin", empleadoLogin);
+        getOrSetUserRol(model, principal);
         return "home";
     }
 
     @GetMapping("/home/empresas")
-    public String empresaTabla(Model model){
+    public String empresaTabla(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         List<Empresa> empresaList = this.empresaService.listEmpresa();
         model.addAttribute("empresaList", empresaList);
         return "empresaTabla";
     }
 
     @GetMapping("/home/empresas/nueva")
-    public String empresa(Model model){
+    public String empresa(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         model.addAttribute("empresa", new Empresa());
         return "empresa";
     }
 
     @GetMapping("/home/empresas/actualizar/{id}")
-    public String empresa(@PathVariable("id") Integer id, Model model){
+    public String empresa(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         model.addAttribute("empresa", empresaService.getEmpresaById(id));
         return "empresaActualizar";
     }
 
     @GetMapping("/home/usuarios")
-    public String usuarioTabla(Model model){
+    public String usuarioTabla(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         List<Empleado> empleadoList = this.empleadoService.listEmpleado();
         model.addAttribute("empleadoList", empleadoList);
         return "usuarioTabla";
     }
 
     @GetMapping("/home/usuarios/nuevo")
-    public String usuario(Model model){
+    public String usuario(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         model.addAttribute("empleado", new Empleado());
         return "usuario";
     }
 
     @GetMapping("/home/usuarios/actualizar/{id}")
-    public String usuario(@PathVariable("id") Integer id, Model model){
+    public String usuario(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         model.addAttribute("empleado", empleadoService.getEmpleadoById(id));
         return "usuarioActualizar";
     }
 
     @GetMapping("/home/transaccion")
-    public String movimientoDineroTabla(Model model){
+    public String movimientoDineroTabla(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         List<MovimientoDinero> movimientoDineroList = this.movimientoDineroService.getMovimientoDineroList();
         model.addAttribute("movimientoList", movimientoDineroList);
         return "movimientoDineroTabla";
     }
 
     @GetMapping("/home/empresas/transacciones/{id}")
-    public String movimientoDineroByEmpresaTabla(@PathVariable("id") int id, Model model){
+    public String movimientoDineroByEmpresaTabla(@PathVariable("id") int id, Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         List<MovimientoDinero> movimientoDineroByEmpresaList = this.movimientoDineroService.getMovimientoDineroByIdEmpresaList(id);
         Empresa empresa = this.empresaService.getEmpresaById(id);
         double totalMonto = 0;
@@ -111,13 +126,15 @@ public class FrontController {
     }
 
     @GetMapping("/home/transaccion/nueva")
-    public String movimientoDinero(Model model){
+    public String movimientoDinero(Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         model.addAttribute("movimiento", new MovimientoDinero());
         return "movimientoDinero";
     }
 
     @GetMapping("/home/transaccion/actualizar/{id}")
-    public String movimientoDinero(@PathVariable("id") Integer id, Model model){
+    public String movimientoDinero(@PathVariable("id") Integer id, Model model, @AuthenticationPrincipal OidcUser principal){
+        getOrSetUserRol(model, principal);
         model.addAttribute("movimiento", movimientoDineroService.getMovimientoDineroById(id));
         return "movimientoDineroActualizar";
     }
